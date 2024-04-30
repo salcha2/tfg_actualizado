3b156053e3f54e9c9c6643be595940da0fa1a52c
Salim Chaieri <salcha2@alum.us.es>
2024-04-30 16:38:30 +0200

    arreglo del crudo

--- cgi/muestra.php
+++ cgi/muestra.php
@@ -4,7 +4,7 @@
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     session_start();
     // Verifica si se han recibido todos los campos esperados
-    if(isset($_SESSION['usuario']) && isset($_POST['result_vulnerability']) && isset($_POST['result_risk']) && isset($_POST['result_functionality']) && isset($_POST['id_v11']) && isset($_POST['id_v21']) && isset($_POST['id_v31']) && isset($_POST['id_v41']) && isset($_POST['id_v51']) && isset($_POST['id_v61']) && isset($_POST['id_v71']) && isset($_POST['id_v81']) && isset($_POST['id_v91']) && isset($_POST['id_v101']) && isset($_POST['id_v111']) && isset($_POST['id_v121']) && isset($_POST['id_v131']) && isset($_POST['id_v141']) && isset($_POST['id_v151']) && isset($_POST['id_v161']) && isset($_POST['id_v171']) && isset($_POST['id_v181']) && isset($_POST['id_v191']) && isset($_POST['id_v201']) && isset($_POST['id_v211'] ) && isset($_POST['latinput']) && isset($_POST['loninput'])) {
+    if (isset($_SESSION['usuario']) && isset($_POST['result_vulnerability']) && isset($_POST['result_risk']) && isset($_POST['result_functionality']) && isset($_POST['id_v11']) && isset($_POST['id_v21']) && isset($_POST['id_v31']) && isset($_POST['id_v41']) && isset($_POST['id_v51']) && isset($_POST['id_v61']) && isset($_POST['id_v71']) && isset($_POST['id_v81']) && isset($_POST['id_v91']) && isset($_POST['id_v101']) && isset($_POST['id_v111']) && isset($_POST['id_v121']) && isset($_POST['id_v131']) && isset($_POST['id_v141']) && isset($_POST['id_v151']) && isset($_POST['id_v161']) && isset($_POST['id_v171']) && isset($_POST['id_v181']) && isset($_POST['id_v191']) && isset($_POST['id_v201']) && isset($_POST['id_v211']) && isset($_POST['latinput']) && isset($_POST['loninput'])) {
         
         // Obtiene los valores de los campos
         $result_vulnerability = $_POST['result_vulnerability'];
@@ -33,36 +33,34 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $id_v21 = $_POST['id_v211'];
         $latitud = $_POST['latinput'];
         $longitud = $_POST['loninput'];
-        $usuario = $_SESSION['usuario']; // Cambiado a $username
+        $usuario = $_SESSION['usuario']; // Cambiado a $usuario
 
-        // Configuración de la conexión a la base de datos PostgreSQL
-        $dsn = "pgsql:host=localhost;port=5432;dbname=Colombia;user=main;password=Soum22";
+        // Verificar si todos los campos están llenos
+        if (!empty($result_vulnerability) && !empty($result_risk) && !empty($result_functionality) && !empty($id_v1) && !empty($id_v2) && !empty($id_v3) && !empty($id_v4) && !empty($id_v5) && !empty($id_v6) && !empty($id_v7) && !empty($id_v8) && !empty($id_v9) && !empty($id_v10) && !empty($id_v11) && !empty($id_v12) && !empty($id_v13) && !empty($id_v14) && !empty($id_v15) && !empty($id_v16) && !empty($id_v17) && !empty($id_v18) && !empty($id_v19) && !empty($id_v20) && !empty($id_v21) && !empty($latitud) && !empty($longitud)) {
+            // Configuración de la conexión a la base de datos PostgreSQL
+            $dsn = "pgsql:host=localhost;port=5432;dbname=Colombia;user=main;password=Soum22";
 
-        try {
-            // Se establece la conexión a la base de datos
-            $dbh = new PDO($dsn);
-            // Habilitar el manejo de errores
-            $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            
-            // Prepara la consulta SQL para insertar los datos en la tabla correspondiente
-            $stmt = $dbh->prepare("INSERT INTO datos4 
-                                    (result_vulnerability, result_risk, result_functionality, id_v1, id_v2, id_v3, id_v4, id_v5, id_v6, id_v7, id_v8, id_v9, id_v10, id_v11, id_v12, id_v13, id_v14, id_v15, id_v16, id_v17, id_v18, id_v19, id_v20, id_v21, coordenadas, usuario) 
-                                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ST_SetSRID(ST_MakePoint(?, ?), 4326), ?)");
-            
-            // Ejecuta la consulta con los valores de los campos del formulario
-            $stmt->execute([$result_vulnerability, $result_risk, $result_functionality, $id_v1, $id_v2, $id_v3, $id_v4, $id_v5, $id_v6, $id_v7, $id_v8, $id_v9, $id_v10, $id_v11, $id_v12, $id_v13, $id_v14, $id_v15, $id_v16, $id_v17, $id_v18, $id_v19, $id_v20, $id_v21, $longitud, $latitud, $usuario]);
-            
-            echo "Los datos se han insertado correctamente en la base de datos";
-        } catch (PDOException $e) {
-            // Si ocurre un error en la conexión o en la consulta, mostrar el mensaje de error
-            echo "Error: " . $e->getMessage();
+            try {
+                // Se establece la conexión a la base de datos
+                $dbh = new PDO($dsn);
+                // Habilitar el manejo de errores
+                $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                // Prepara la consulta SQL para insertar los datos en la tabla correspondiente
+                $stmt = $dbh->prepare("INSERT INTO datos4 
+                                        (result_vulnerability, result_risk, result_functionality, id_v1, id_v2, id_v3, id_v4, id_v5, id_v6, id_v7, id_v8, id_v9, id_v10, id_v11, id_v12, id_v13, id_v14, id_v15, id_v16, id_v17, id_v18, id_v19, id_v20, id_v21, coordenadas, usuario) 
+                                        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ST_SetSRID(ST_MakePoint(?, ?), 4326), ?)");
+                
+                // Ejecuta la consulta con los valores de los campos del formulario
+                $stmt->execute([$result_vulnerability, $result_risk, $result_functionality, $id_v1, $id_v2, $id_v3, $id_v4, $id_v5, $id_v6, $id_v7, $id_v8, $id_v9, $id_v10, $id_v11, $id_v12, $id_v13, $id_v14, $id_v15, $id_v16, $id_v17, $id_v18, $id_v19, $id_v20, $id_v21, $longitud, $latitud, $usuario]);
+                
+                echo "Los datos se han insertado correctamente en la base de datos";
+            } catch (PDOException $e) {
+                // Si ocurre un error en la conexión o en la consulta, mostrar el mensaje de error
+                echo "Error: " . $e->getMessage();
+            }
         }
-    } else {
-        // Si no se reciben todos los campos esperados, muestra un mensaje de error
-        echo json_encode('Error: No se han recibido todos los campos esperados');
     }
-} else {
-    // Si la solicitud no es de tipo POST, muestra un mensaje de error
-    echo json_encode('Error: La solicitud no es de tipo POST');
 }
+
 ?>
